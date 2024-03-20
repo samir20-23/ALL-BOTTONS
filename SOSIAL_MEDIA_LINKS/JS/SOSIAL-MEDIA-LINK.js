b39039715c5efd40e31e9793c0b77983a60c482b
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-03-20 17:37:07 +0000

    Add files via upload

---
+++ SOSIAL_MEDIA_LINKS/JS/SOSIAL-MEDIA-LINK.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.share-button').addEventListener('click', function(){
+ let sosial = document.querySelector('.socials-wrapper')
+    sosial.classList.toggle('active')
+
+    
+
+});
